bb61a4d6a94a8a320f81a8850a83ef5a141399e9
Charlie <ctslone27@gmail.com>
2020-06-29 18:54:14 -0400

    added init function and confirmation for user. added input function which is for asking users what content they want to include.

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 var inqurer = require("inquirer");
+const inquirer = require("inquirer");
+
+function initialize() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Do you want to create a README.md file?",
+            name: "userConfirmation"
+        }
+    ]).then(function(response) {
+        if(response.userConfirmation) {
+            getInput()
+        }
+        else {
+            console.log("Goodbye.")
+        }
+    })
+}
+
+function getInput() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the title.",
+            name: "userTitle"
+        },
+        {
+            type: "input",
+            message: "Enter the tagline of your application.",
+            name: "userTagline"
+        }
+    ])
+}
+
+
+initialize()
